0895fa205876ea01ea7a2e6a867a32bf5e0f07ad
Udayk02 <udaykirankarusodi@gmail.com>
2025-07-22 21:51:14 +0530

    initial commit

---
+++ src/main/java/com/uk02/rmw/RmwApplication.java
@@ -0,0 +1,13 @@
+package com.uk02.rmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uk02/rmw/RmwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uk02.rmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
