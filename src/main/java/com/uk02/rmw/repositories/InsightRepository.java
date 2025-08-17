7e8d1f99ede7f1c51126e9e1246e11ee5d436c9e
Udayk02 <udaykirankarusodi@gmail.com>
2025-08-17 20:17:27 +0530

    add repository tag to InsightRepository

--- src/main/java/com/uk02/rmw/repositories/InsightRepository.java
+++ src/main/java/com/uk02/rmw/repositories/InsightRepository.java
@@ -3,9 +3,11 @@ package com.uk02.rmw.repositories;
 import com.uk02.rmw.models.Insight;
 import com.uk02.rmw.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InsightRepository extends JpaRepository<Insight, Long> {
     List<Insight> findTop10ByUserOrderByIdDesc(User user);
 }
